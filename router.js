cb4929b6208dbe5523742d02bcc808fb655d891a
elpenao <elpenao@gmail.com>
2015-12-22 11:56:37 -0600

    finished

---
+++ router.js
@@ -0,0 +1,62 @@
+var Profile = require('./profile.js');
+var http = require('http');
+var renderer = require('./renderer.js');
+var querystring = require('querystring');
+
+var commonHeader = {'Content-Type': 'text/html'};
+
+function home(request, response) {
+	if (request.url === '/') {
+		if (request.method.toLowerCase() === "get") {
+			// show search
+			response.writeHead(200, commonHeader);
+			renderer.view('header', {}, response);
+			renderer.view('search', {}, response);
+			renderer.view('footer',{}, response);
+			response.end();
+		} else {
+			request.on('data', function(postBody){
+				var query = querystring.parse(postBody.toString());
+				response.writeHead(303, {"Location": '/' + query.username});
+				response.end();
+			});
+		}
+	}
+}
+
+function user(request, response) {
+	var username = request.url.replace('/','');
+	var studentProfile = new Profile(username);
+	if (username.length > 0 ) {
+		response.writeHead(200, commonHeader);
+		renderer.view('header', {}, response);
+
+		
+		studentProfile.on('end', function(profileJSON){
+			// show profile
+
+			// store values which we need
+			var values = {
+				avatarUrl: profileJSON.gravatar_url,
+				username: profileJSON.profile_name,
+				badges: profileJSON.badges.length,
+				javascriptPoints: profileJSON.points.JavaScript
+			}
+			// simple response
+			renderer.view('profile',values,response);
+			renderer.view('footer',{}, response);
+			response.end();
+		});		
+	}
+	// on error
+	studentProfile.on('error', function(error){
+		//show error
+		renderer.view('error', {errorMessage: error.message}, response);
+		renderer.view('search', {}, response);
+		renderer.view('footer',{}, response);
+		response.end();
+	});
+}
+
+module.exports.home = home;
+module.exports.user = user;
